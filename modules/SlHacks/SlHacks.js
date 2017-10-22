644a47137eff46b4e176899597fea0cd71ccd981
Iliyan Georgiev <ilian_ge@abv.bg>
2017-10-22 17:45:43 +0300

    Make SL bar buttons match cell colors

--- modules/SlHacks/SlHacks.js
+++ modules/SlHacks/SlHacks.js
@@ -1,4 +1,10 @@
 function SlHacks() {
+    function setColor(selector, color) {
+        $(selector).each(function() {
+            this.style.setProperty("color", color, "important");
+        });
+    }
+
     var intervalId = window.setInterval(function () {
         // Stop SL bug with R toggling review mode
         if (!document.getElementById("slPanel")) return;
@@ -8,6 +14,10 @@ function SlHacks() {
         });
         console.log("Fixed Scouts' Log Review button bug.");
         window.clearInterval(intervalId);
+
+        // Set Need Scythe/Admin button colors to match cell colors
+        setColor(".sl-need-admin", Cell.ScytheVisionColors.reap);
+        setColor(".sl-need-scythe", Cell.ScytheVisionColors.scythed);
 
     }, 1000);
 }
