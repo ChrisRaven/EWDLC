ee20d6d36a71725fe686eb29d2a91f67b32090a1
Krzysztof Kruk <kr.kruk@gmail.com>
2018-08-22 22:33:34 +0200

    Reorganized settings

--- src/modules/tabbedChat/TabbedPrefs.js
+++ src/modules/tabbedChat/TabbedPrefs.js
@@ -3,28 +3,28 @@ import {Setting} from "../../framework/Setting.js"
 function TabbedPrefs(callback) {
     var settings = {
         "tc-show-timestamp": new Setting("tc-show-timestamp", true),
-        "tc-show-all-in-tabs": new Setting("tc-show-all-in-tabs", true),
         "tc-show-points-msgs": new Setting("tc-show-points-msgs", true),
+        "tc-show-all-in-tabs": new Setting("tc-show-all-in-tabs", true),
         "tc-show-channels-in-all": new Setting("tc-show-channels-in-all", true),
         "tc-enable-unread": new Setting("tc-enable-unread", true),
-        "tc-grayout-messages": new Setting("tc-grayout-messages", false),
         "tc-show-leaderboard": new Setting("tc-show-leaderboard", true),
+        "tc-grayout-messages": new Setting("tc-grayout-messages", false),
         "tc-allow-backslash-prefix": new Setting("tc-allow-backslash-prefix", true),
         "tc-enable-markup": new Setting("tc-enable-markup", true),
         "tc-show-connection-statuses": new Setting("tc-show-connection-statuses", false)
     };
 
     var lang = [
-        {key: "tc-show-timestamp", lang: "Chat Timestamp"},
-        {key: "tc-show-all-in-tabs", lang: "General Chat visible in all channels"},
-        {key: "tc-show-channels-in-all", lang: "All channels visible in General Chat"},
-        {key: "tc-show-points-msgs", lang: "Points messages"},
-        {key: "tc-enable-unread", lang: "Unread messages counter"},
-        {key: "tc-grayout-messages", lang: "Show all hidden messages as faded instead"},
-        {key: "tc-show-leaderboard", lang: "Leaderboard pop-up after cube submission"},
-        {key: "tc-allow-backslash-prefix", lang: "Allow backslash as command prefix"},
-        {key: "tc-enable-markup", lang: "Enable markup"},
-        {key: "tc-show-connection-statuses", lang: "Show connection statuses"}
+        {key: "tc-show-timestamp", lang: "Timestamps"},
+        {key: "tc-show-points-msgs", lang: "Points Messages"},
+        {key: "tc-show-all-in-tabs", lang: "General Chat visible in tabs"},
+        {key: "tc-show-channels-in-all", lang: "Group Chats visible in General"},
+        {key: "tc-enable-unread", lang: "Unread Messages Counter"},
+        {key: "tc-show-leaderboard", lang: "Leaderboard after cube submission"},
+        {key: "tc-grayout-messages", lang: "All hidden messages as faded"},
+        {key: "tc-allow-backslash-prefix", lang: "Backslash as command prefix"},
+        {key: "tc-enable-markup", lang: "Markup"},
+        {key: "tc-show-connection-statuses", lang: "Connection statuses"}
     ]
 
     var _this = this;
